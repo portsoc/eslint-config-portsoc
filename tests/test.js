ae69a631431fa542a7f1c4d4593f6b3365db66a1
Jacek Kopecky <jacek@jacek.cz>
2020-03-19 20:15:42 +0000

    add skeleton tests

---
+++ tests/test.js
@@ -0,0 +1,13 @@
+/* global QUnit */
+'use strict';
+
+
+QUnit.test(
+  'test',
+  function (assert) {
+    assert.ok(1, 'foo');
+  });
+
+
+// todo test all the JS files in this repo
+// todo test files in tests/files bad and good; shout if neither good or bad
