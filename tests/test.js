5801bd3fd50f3d6eb3e0286212a997790635d51c
Jacek Kopecky <jacek@jacek.cz>
2020-03-26 17:44:28 +0000

    treat every file as a test case

--- tests/test.js
+++ tests/test.js
@@ -11,30 +11,36 @@ const FILES_DIR = path.join(__dirname, 'files');
 const BAD_SUFFIX = '-bad.js';
 const GOOD_SUFFIX = '-good.js';
 
-QUnit.test('test files', function (assert) {
-  const linter = new eslint.CLIEngine({ configFile: 'index.js' });
+const linter = new eslint.CLIEngine({ configFile: 'index.js' });
 
-  const files = fs.readdirSync(FILES_DIR);
+const files = fs.readdirSync(FILES_DIR);
+QUnit.test('there are testing files', assert => {
   assert.notEqual(files.length, 0, 'there should be some test files');
-
   for (const file of files) {
     assert.ok(file.endsWith(BAD_SUFFIX) || file.endsWith(GOOD_SUFFIX),
       `test files must end with ${BAD_SUFFIX} or ${GOOD_SUFFIX}`);
+  }
+});
 
-    const text = fs.readFileSync(path.join(FILES_DIR, file), 'utf8');
-    const report = linter.executeOnText(text);
-    const messages = report.results[0].messages;
+for (const file of files) {
+  if (file.endsWith(BAD_SUFFIX)) {
+    QUnit.test(`file ${file} should fail linting`, assert => {
+      const text = fs.readFileSync(path.join(FILES_DIR, file), 'utf8');
+      const report = linter.executeOnText(text);
+      const messages = report.results[0].messages;
 
-    if (file.endsWith(BAD_SUFFIX)) {
       assert.notEqual(messages.length, 0, `file ${file} should be bad but lints as good`);
-    } else {
+    });
+  } else {
+    QUnit.test(`file ${file} should be good`, assert => {
+      const text = fs.readFileSync(path.join(FILES_DIR, file), 'utf8');
+      const report = linter.executeOnText(text);
+      const messages = report.results[0].messages;
+
       assert.equal(messages.length, 0, `file ${file} should be good but has linting errors`);
       if (messages.length > 0) {
         console.log({ file, messages });
       }
-    }
+    });
   }
-});
-
-
-// todo test files in tests/files bad and good; shout if neither good or bad
+}
