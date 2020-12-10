8a1238b78f25758821e3cfdf0ccad6999645299c
Jacek Kopecky <jacek@jacek.cz>
2020-12-10 18:54:54 +0000

    disallow if(cond); without else

---
+++ tests/files/if-empty-block-bad.js
@@ -0,0 +1,4 @@
+let i = 1;
+if (i < 2);
+// if with semicolon and without else is a likely mistake, never useful.
+i += 1;
---
+++ tests/files/if-empty-block-good.js
@@ -0,0 +1,6 @@
+let i = 1;
+if (i < 2);
+else {
+  // if with semicolon is sometimes OK if followed by else
+  i += 1;
+}
