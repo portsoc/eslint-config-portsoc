e3189321f41ff83fe84941a2a925294ce7b6dc88
theojeanb <theojeanb@gmail.com>
2021-04-15 12:52:00 +0200

    Added TS overrides

--- index.js
+++ index.js
@@ -58,4 +58,64 @@ module.exports = {
     'import/no-unresolved': [2, { commonjs: true }],
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'plugin:@typescript-eslint/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      settings: {
+        'import/resolver': {
+          node: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          },
+        },
+      },
+      rules: {
+        'indent': ['off'],
+        '@typescript-eslint/indent': ['error', 2],
+        'no-shadow': ['off'],
+        '@typescript-eslint/no-shadow': ['error'],
+        'default-case': ['off'],
+        '@typescript-eslint/member-delimiter-style': ['error', {
+          multiline: {
+            delimiter: 'comma',
+            requireLast: true,
+          },
+          singleline: {
+            delimiter: 'comma',
+            requireLast: false,
+          },
+        }],
+        'no-param-reassign': [0],
+        'no-restricted-syntax': [0],
+        'prefer-destructuring': [0],
+        'no-else-return': [0],
+        'no-console': [0],
+        'no-continue': [0],
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': ['error', { functions: false }],
+        'jsx-a11y/no-autofocus': [0],
+        'no-cond-assign': ['error', 'except-parens'],
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+        'import/prefer-default-export': [0],
+        'import/extensions': [
+          'error',
+          'always',
+          {
+            ts: 'never',
+            tsx: 'never',
+            js: 'never',
+            jsx: 'never',
+          },
+        ],
+        'no-underscore-dangle': [0],
+        'no-mixed-operators': [0],
+        'lines-between-class-members': ['error', 'always', { exceptAfterSingleLine: true }],
+      },
+    },
+  ],
 };
