f9d28e4f671e9dc29190b706f41f92653e448971
Jacek Kopecky <jacek@jacek.cz>
2022-01-27 17:55:14 +0000

    update tests to eslint 8 API

--- tests/test.js
+++ tests/test.js
@@ -4,14 +4,14 @@
 const fs = require('fs');
 const path = require('path');
 
-const eslint = require('eslint');
+const { ESLint } = require('eslint');
 
 const FILES_DIR = path.join(__dirname, 'files');
 
 const BAD_SUFFIX = '-bad.js';
 const GOOD_SUFFIX = '-good.js';
 
-const linter = new eslint.CLIEngine({ configFile: 'index.js' });
+const linter = new ESLint({ overrideConfigFile: 'index.js' });
 
 const files = fs.readdirSync(FILES_DIR);
 QUnit.test('there are testing files', assert => {
@@ -25,18 +25,18 @@ QUnit.test('there are testing files', assert => {
 // set up a QUnit test for each file
 for (const file of files) {
   if (file.endsWith(BAD_SUFFIX)) {
-    QUnit.test(`file ${file} should fail linting`, assert => {
+    QUnit.test(`file ${file} should fail linting`, async (assert) => {
       const text = fs.readFileSync(path.join(FILES_DIR, file), 'utf8');
-      const report = linter.executeOnText(text);
-      const messages = report.results[0].messages;
+      const results = await linter.lintText(text);
+      const messages = results[0].messages;
 
       assert.notEqual(messages.length, 0, 'should report linting errors');
     });
   } else {
-    QUnit.test(`file ${file} should be good`, assert => {
+    QUnit.test(`file ${file} should be good`, async (assert) => {
       const text = fs.readFileSync(path.join(FILES_DIR, file), 'utf8');
-      const report = linter.executeOnText(text);
-      const messages = report.results[0].messages;
+      const results = await linter.lintText(text);
+      const messages = results[0].messages;
 
       assert.equal(messages.length, 0, 'should report no linting errors');
       if (messages.length > 0) {
